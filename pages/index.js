add488cea2965a2c9f6d7a31319c409990c74a85
emiri yasuo <emiri.yasuo@rakus-partners.co.jp>
2022-08-15 14:30:10 +0900

    ページの作成（最初の投稿、リンクの作成）

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -12,8 +13,14 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        {/* <h1 className={styles.title}>
           Learn <a href="https://nextjs.org">Next.js!</a>
+        </h1> */}
+        <h1 className={styles.title}>
+        <Link href="/posts/first-post">
+        <a>このページ</a>
+        </Link>{' '}
+        を読もう！
         </h1>
 
         <p className={styles.description}>
